f3d16305bd0ea9f6439fb5678c2af5db3668d322
amatureApe <amatureApe@gmail.com>
2023-05-17 21:30:56 -0700

    add one more example

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -18,9 +18,23 @@ fn main() {
     println!("{}", concatenated_string);
 
     println!("--------------------");
+
+    control_flow(50);
 }
 
 fn concat_string(input: &str) -> String {
     let result = String::from(input) + " World";
     result
+}
+
+fn control_flow(x: i32) {
+    if x == 1 {
+        println!("The value is 1")
+    } else if x > 49 {
+        println!("The value is greater than equal to 50");
+    } else if x < 25 {
+        println!("The value is less than 25");
+    } else {
+        println!("The value is greater than 25 but less than 50");
+    }
 }
