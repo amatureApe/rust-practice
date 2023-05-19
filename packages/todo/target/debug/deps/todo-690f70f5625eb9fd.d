99bf4f9437d9f03cca2bf2529bba0df0ec3b8e27
amatureApe <amatureApe@gmail.com>
2023-05-19 16:35:08 -0700

    add modules

---
+++ packages/todo/target/debug/deps/todo-690f70f5625eb9fd.d
@@ -0,0 +1,7 @@
+/Users/dariantaban/Documents/coding/projects/rust_practice/packages/todo/target/debug/deps/todo-690f70f5625eb9fd.rmeta: src/lib.rs
+
+/Users/dariantaban/Documents/coding/projects/rust_practice/packages/todo/target/debug/deps/libtodo-690f70f5625eb9fd.rlib: src/lib.rs
+
+/Users/dariantaban/Documents/coding/projects/rust_practice/packages/todo/target/debug/deps/todo-690f70f5625eb9fd.d: src/lib.rs
+
+src/lib.rs:
---
+++ packages/todo/target/debug/libtodo.d
@@ -0,0 +1,1 @@
+/Users/dariantaban/Documents/coding/projects/rust_practice/packages/todo/target/debug/libtodo.rlib: /Users/dariantaban/Documents/coding/projects/rust_practice/packages/todo/src/lib.rs
