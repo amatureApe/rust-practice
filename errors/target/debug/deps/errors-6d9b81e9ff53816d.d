2179da24049544b9d234a6e05942c5ad6e2cc5bb
amatureApe <amatureApe@gmail.com>
2023-05-27 04:28:51 -0700

    collections

---
+++ errors/target/debug/deps/errors-6d9b81e9ff53816d.d
@@ -0,0 +1,5 @@
+/Users/dariantaban/Documents/coding/projects/rust_practice/errors/target/debug/deps/errors-6d9b81e9ff53816d: src/main.rs
+
+/Users/dariantaban/Documents/coding/projects/rust_practice/errors/target/debug/deps/errors-6d9b81e9ff53816d.d: src/main.rs
+
+src/main.rs:
---
+++ errors/target/debug/errors.d
@@ -0,0 +1,1 @@
+/Users/dariantaban/Documents/coding/projects/rust_practice/errors/target/debug/errors: /Users/dariantaban/Documents/coding/projects/rust_practice/errors/src/main.rs
