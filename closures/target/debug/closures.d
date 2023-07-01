067475265545de7941bfac1bc7d034bc52c46c29
amatureApe <amatureApe@gmail.com>
2023-06-30 20:00:04 -0700

    closures

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/Users/dariantaban/Documents/coding/projects/rust_practice/closures/target/debug/closures: /Users/dariantaban/Documents/coding/projects/rust_practice/closures/src/main.rs
---
+++ closures/target/debug/deps/closures-56aac347f34454c3.d
@@ -0,0 +1,5 @@
+/Users/dariantaban/Documents/coding/projects/rust_practice/closures/target/debug/deps/closures-56aac347f34454c3: src/main.rs
+
+/Users/dariantaban/Documents/coding/projects/rust_practice/closures/target/debug/deps/closures-56aac347f34454c3.d: src/main.rs
+
+src/main.rs:
